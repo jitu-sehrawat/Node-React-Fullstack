65b3dc2a8b23eecb11bbdf54e8ad20c5d64edcbe
Jitender Sehrawat <jitender.sehrawat@adglobal360.com>
2019-12-28 01:36:04 +0530

    env set

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// Production keys here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleCallbackURL: process.env.GOOGLE_CALLBACK_URI,
+  mongoURI: process.env.MONGO_URI,
+  cookieSession: process.env.COOKIE_SESSION
+};
